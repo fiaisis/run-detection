6e258966cd339124bccf994e5650d87caf28f02a
Keiran Price <keiran.price@stfc.ac.uk>
2022-12-05 12:32:33 +0000

    update main

--- main.py
+++ main.py
@@ -2,13 +2,23 @@
 Main Module
 """
 
+from multiprocessing import SimpleQueue, Process
 
-def main() -> None:
+from src.queue_listener import QueueListener
+from src.run_detection import RunDetector
+
+
+def main():
     """
     run-detection entrypoint.
     :return: None
     """
-    print("Hello world")
+    message_queue = SimpleQueue()
+    ack_queue = SimpleQueue()
+    run_detector = RunDetector(message_queue, ack_queue)
+    queue_listener = QueueListener(message_queue, ack_queue)
+    Process(target=queue_listener.run).start()
+    run_detector.run()
 
 
 if __name__ == "__main__":
