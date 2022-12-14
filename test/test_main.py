ca1320eae79277ac8bf94468f727305613962686
Samuel Jones <samuel.jones@stfc.ac.uk>
2022-12-14 12:19:21 +0000

    Update tests for test_main

--- test/test_main.py
+++ test/test_main.py
@@ -8,24 +8,35 @@ from unittest import mock
 from rundetection.run_detection import main
 
 
-class TestMain(unittest.TestCase):
-    def setUp(self):
-        self.amq_ip = "great_ip"
-        self.amq_user = "great_username"
-        self.amq_pass = "great_password"
+@mock.patch("rundetection.run_detection.RunDetector")
+def test_main_uses_activemq_env_vars(run_detector) -> None:
+    """
+    Testing that run_detector.run receives the correct variables from the environment
+    """
+    amq_ip = "great_ip"
+    amq_user = "great_username"
+    amq_pass = "great_password"
+    os.environ["ACTIVEMQ_IP"] = amq_ip
+    os.environ["ACTIVEMQ_USER"] = amq_user
+    os.environ["ACTIVEMQ_PASS"] = amq_pass
 
-        os.environ["ACTIVEMQ_IP"] = self.amq_ip
-        os.environ["ACTIVEMQ_USER"] = self.amq_user
-        os.environ["ACTIVEMQ_PASS"] = self.amq_pass
+    main()
 
-    @mock.patch("rundetection.run_detection.RunDetector")
-    def test_main_uses_activemq_env_vars(self, run_detector) -> None:
-        """
-        Testing that run_detector.run receives the correct variables from the environment
-        """
-        main()
+    run_detector.return_value.run.assert_called_once_with(amq_ip, amq_user, amq_pass)
 
-        run_detector.return_value.run.assert_called_once_with(self.amq_ip, self.amq_user, self.amq_pass)
+
+@mock.patch("rundetection.run_detection.RunDetector")
+def test_main_uses_useful_defaults_when_activemq_env_vars_not_set(run_detector) -> None:
+    """
+    Testing that run_detector.run uses useful defaults when env variables are not set
+    """
+    del os.environ["ACTIVEMQ_IP"]
+    del os.environ["ACTIVEMQ_USER"]
+    del os.environ["ACTIVEMQ_PASS"]
+
+    main()
+
+    run_detector.return_value.run.assert_called_once_with("localhost", "admin", "admin")
 
 
 if __name__ == '__main__':
