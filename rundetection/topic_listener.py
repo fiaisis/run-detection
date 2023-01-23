b17c6315846f52dcd1439cdc93c048d41e7fcb4a
Samuel Jones <samjones714@gmail.com>
2023-01-23 09:52:41 +0000

    Update rundetection/topic_listener.py

--- rundetection/topic_listener.py
+++ rundetection/topic_listener.py
@@ -62,7 +62,7 @@ class TopicListener(ConnectionListener):  # type: ignore # No Library stub
             logger.info("Attempting connection")
             self._connection.connect(username=self._user, password=self._password)
             self._connection.set_listener(listener=self, name="run-detection-listener")
-            self._connection.subscribe(destination="/topic/Interactive-Reduction", id=self._subscription_id,
+            self._connection.subscribe(destination="Interactive-Reduction", id=self._subscription_id,
                                        ack="client")
         except ConnectFailedException:
             logger.warning("Failed to reconnect, attempting again in 30 seconds")
