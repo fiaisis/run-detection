281dded767e77cb3181126ff579b61a2170d5980
Keiran Price <keiran.price@stfc.ac.uk>
2023-02-16 11:26:35 +0000

    Remove uneeded rule

--- rundetection/rules/common_rules.py
+++ rundetection/rules/common_rules.py
@@ -13,13 +13,3 @@ class EnabledRule(Rule[bool]):
 
     def verify(self, run: DetectedRun) -> None:
         run.will_reduce = self._value
-
-
-class SpecificNameRule(Rule[str]):
-    """
-    Rule concretion for ensuring that the run filename contains a given value. If the filepath contains the name,
-    the run will be reduced, if not it will be skipped
-    """
-
-    def verify(self, run: DetectedRun) -> None:
-        run.will_reduce = self._value in run.filepath.name
