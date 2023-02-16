fab9435d1efdbfd3a3ef35978ce4a304920a934c
Keiran Price <keiran.price@stfc.ac.uk>
2023-02-16 11:20:48 +0000

    Add function to get sibling files

--- rundetection/ingest.py
+++ rundetection/ingest.py
@@ -1,11 +1,12 @@
 """
-Ingest module holds the NexusMetadata class and the ingest function used to build NexusMetadata from nexus files
+Ingest module holds the DetectedRun class and the ingest function used to build DetectedRumns from nexus files
 """
 import dataclasses
 import json
 import logging
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Dict, Any, List
 
 from h5py import File  # type: ignore
 
@@ -15,15 +16,16 @@ logger = logging.getLogger(__name__)
 @dataclass
 class DetectedRun:
     """
-    DetectedRun 
+    DetectedRun
     """
+
     run_number: int
     instrument: str
     experiment_title: str
     experiment_number: str
-    filepath: str
+    filepath: Path
     will_reduce: bool = True
-    additional_values: Dict[str, any] = dataclasses.field(default_factory=dict)
+    additional_values: Dict[str, Any] = dataclasses.field(default_factory=dict)
 
     def to_json_string(self) -> str:
         """
@@ -37,7 +39,7 @@ def ingest(path: Path) -> DetectedRun:
     """
     Given the path of a nexus file, Create and return a DetectedRun
     :param path: The path of the nexus file
-    :return: The NexusMetadata of the given nexus file
+    :return: The DetectedRun built from the given nexus file
     """
     logger.info("Ingesting file: %s", path)
     try:
@@ -49,7 +51,7 @@ def ingest(path: Path) -> DetectedRun:
             instrument=dataset.get("beamline")[0].decode("utf-8"),
             experiment_title=dataset.get("title")[0].decode("utf-8"),
             experiment_number=dataset.get("experiment_identifier")[0].decode("utf-8"),
-            filepath=str(path),
+            filepath=path,
         )
 
         logger.info("extracted metadata: %s", detection_result)
@@ -57,3 +59,21 @@ def ingest(path: Path) -> DetectedRun:
     except FileNotFoundError:
         logger.error("Nexus file could not be found: %s", path)
         raise
+
+
+def get_sibling_nexus_files(nexus_path: Path) -> List[Path]:
+    """
+    Given the path of a nexus file, return a list of any other nexus files in the same directory
+    :param nexus_path: The nexus file for which directory to search
+    :return: List of sibling nexus files
+    """
+    return [Path(file) for file in nexus_path.parents[0].glob("*.nxs") if Path(file) != nexus_path]
+
+
+def get_sibling_runs(nexus_path: Path) -> List[DetectedRun]:
+    """
+    Given the path of a nexus file, return a list of ingested sibling nexus files in the same directory
+    :param nexus_path: The nexus file for which directory to search
+    :return: List of DetectedRun Objects
+    """
+    return [ingest(file) for file in get_sibling_nexus_files(nexus_path)]
--- test/test_ingest.py
+++ test/test_ingest.py
@@ -3,42 +3,44 @@ Ingest and metadata tests
 """
 import unittest
 from pathlib import Path
+from tempfile import TemporaryDirectory
 from typing import Tuple, List
+from unittest.mock import Mock, patch
 
 import pytest
 from _pytest.logging import LogCaptureFixture
 
-from rundetection.ingest import NexusMetadata, ingest
+from rundetection.ingest import ingest, DetectedRun, get_sibling_nexus_files, get_sibling_runs
 
-TEST_FILE_METADATA_PAIRS: List[Tuple[str, NexusMetadata]] = [
+TEST_FILE_METADATA_PAIRS: List[Tuple[str, DetectedRun]] = [
     (
         "e2e_data/1510111/ENGINX00241391.nxs",
-        NexusMetadata(
+        DetectedRun(
             run_number=241391,
             instrument="ENGINX",
             experiment_title="CeO2 4 x 4 x 15",
             experiment_number="1510111",
-            filepath="test/test_data/e2e_data/1510111/ENGINX00241391.nxs",
+            filepath=Path("test/test_data/e2e_data/1510111/ENGINX00241391.nxs"),
         ),
     ),
     (
         "e2e_data/1600007/IMAT00004217.nxs",
-        NexusMetadata(
+        DetectedRun(
             run_number=4217,
             instrument="IMAT",
             experiment_title="Check DAE and end of run working after move",
             experiment_number="1600007",
-            filepath="test/test_data/e2e_data/1600007/IMAT00004217.nxs",
+            filepath=Path("test/test_data/e2e_data/1600007/IMAT00004217.nxs"),
         ),
     ),
     (
         "e2e_data/1920302/ALF82301.nxs",
-        NexusMetadata(
+        DetectedRun(
             run_number=82301,
             instrument="ALF",
             experiment_title="YbCl3 rot=0",
             experiment_number="1920302",
-            filepath="test/test_data/e2e_data/1920302/ALF82301.nxs",
+            filepath=Path("test/test_data/e2e_data/1920302/ALF82301.nxs"),
         ),
     ),
 ]
@@ -62,7 +64,7 @@ def test_to_json_string() -> None:
     Test valid json string can be built from metadata
     :return: None
     """
-    nexus_metadata = NexusMetadata(
+    nexus_metadata = DetectedRun(
         run_number=12345,
         instrument="LARMOR",
         experiment_number="54321",
@@ -71,7 +73,9 @@ def test_to_json_string() -> None:
     )
     assert (
         nexus_metadata.to_json_string() == '{"run_number": 12345, "instrument": "LARMOR", "experiment_title": '
-        '"my experiment", "experiment_number": "54321", "filepath": "e2e_data/1920302/ALF82301.nxs"}'
+        '"my experiment", "experiment_number": "54321", "filepath": '
+        '"e2e_data/1920302/ALF82301.nxs", "will_reduce": true, '
+        '"additional_values": {}}'
     )
 
 
@@ -85,6 +89,25 @@ def test_logging_and_exception_when_nexus_file_does_not_exit(caplog: LogCaptureF
         ingest(Path("e2e_data/foo/bar.nxs"))
 
     assert "Nexus file could not be found: e2e_data/foo/bar.nxs" in caplog.text
+
+
+def test_get_sibling_nexus_files():
+    with TemporaryDirectory() as td:
+        Path(td, "1.nxs").touch()
+        Path(td, "2.nxs").touch()
+        Path(td, "1.log").touch()
+        sibling_files = get_sibling_nexus_files(Path(td, "1.nxs"))
+        assert sibling_files == [Path(td, "2.nxs")]
+
+
+@patch("rundetection.ingest.ingest")
+def test_get_sibling_runs(mock_ingest: Mock):
+    run = DetectedRun(1, "inst", "title", "num", "path")
+    mock_ingest.return_value = run
+    with TemporaryDirectory() as td:
+        Path(td, "1.nxs").touch()
+        Path(td, "2.nxs").touch()
+        assert get_sibling_runs(Path(td, "1.nxs")) == [run]
 
 
 if __name__ == "__main__":
