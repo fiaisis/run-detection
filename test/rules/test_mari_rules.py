6425d2179a80509eaecb280bc84c4ef0e1faced8
Keiran Price <keiran.price@stfc.ac.uk>
2023-05-02 15:56:30 +0100

    pylint fixtures

--- test/rules/test_mari_rules.py
+++ test/rules/test_mari_rules.py
@@ -1,3 +1,7 @@
+"""
+Test for mari rules
+"""
+# pylint:disable = redefined-outer-name
 from pathlib import Path
 from unittest.mock import patch
 
@@ -9,6 +13,10 @@ from rundetection.rules.mari_rules import MariStitchRule, MariMaskFileRule
 
 @pytest.fixture
 def detected_run():
+    """
+    Detected run fixture
+    :return: Detected run
+    """
     return DetectedRun(
         run_number=100,
         filepath=Path("/archive/100/MARI100.nxs"),
@@ -27,15 +35,27 @@ def detected_run():
 
 @pytest.fixture
 def mari_stitch_rule_true():
+    """
+    stitch rule fixture for true
+    :return: MariStitchRule
+    """
     return MariStitchRule(value=True)
 
 
 @pytest.fixture
 def mari_stitch_rule_false():
+    """
+    Stitch rule fixture for false
+    :return: MariStitchRule
+    """
     return MariStitchRule(value=False)
 
 
 def test_get_previous_run_path():
+    """
+    Test previous path is obtained
+    :return: None
+    """
     run_path = Path("/archive/MARI100.nxs")
     run_number = 100
 
@@ -48,6 +68,12 @@ def test_get_previous_run_path():
 
 
 def test_verify_with_stitch_rule_false(mari_stitch_rule_false, detected_run):
+    """
+    Test not added when none to stitch
+    :param mari_stitch_rule_false: rule fixture
+    :param detected_run: run fixture
+    :return: none
+    """
     mari_stitch_rule_false.verify(detected_run)
     assert not detected_run.additional_runs
 
@@ -55,11 +81,24 @@ def test_verify_with_stitch_rule_false(mari_stitch_rule_false, detected_run):
 @patch("rundetection.ingest.get_run_title", return_value="Test experiment")
 @patch("pathlib.Path.exists", return_value=False)
 def test_verify_with_single_run(_, __, mari_stitch_rule_true, detected_run):
+    """
+    Test not added for single run
+    :param _: unused mock path
+    :param __: unused mock get title
+    :param mari_stitch_rule_true: stitch rule fixture
+    :param detected_run: detected run fixture
+    :return: none
+    """
     mari_stitch_rule_true.verify(detected_run)
     assert not detected_run.additional_runs
 
 
 def test_mari_mask_rule(detected_run):
+    """
+    Test given link is attached to additional values
+    :param detected_run: detected run fixture
+    :return: none
+    """
     rule = MariMaskFileRule("some link")
     rule.verify(detected_run)
 
