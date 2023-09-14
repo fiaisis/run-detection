438b656a2c311ba4980573c7381e1fee1605965e
Keiran Price <keiran.price@stfc.ac.uk>
2023-09-14 09:05:01 +0100

    Add rule test

---
+++ test/rules/test_tosca_rules.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+from unittest.mock import Mock, patch
+
+import pytest
+
+from rundetection.ingest import JobRequest
+from rundetection.rules.tosca_rules import ToscaStitchRule
+
+
+@pytest.fixture()
+def job_request():
+    """job_request fixture"""
+    return JobRequest(
+        run_number=12345,
+        instrument="instrument",
+        experiment_title="experiment title run 1",
+        filepath=Path("./25581.nxs"),
+        experiment_number="experiment number",
+        raw_frames=0,
+        good_frames=0,
+        run_start="",
+        run_end="",
+        users="",
+    )
+
+
+def test_is_title_similar():
+    """
+    Test similar titles will be correctly identified
+    :return: None
+    """
+    assert ToscaStitchRule._is_title_similar("some long run run-1", "some long run run 2")
+    assert not ToscaStitchRule._is_title_similar("some other run run-1", "different run completely")
+
+
+@patch("rundetection.rules.tosca_rules.ToscaStitchRule._get_runs_to_stitch")
+def test_stitch_rule_does_nothing_if_disabled(mock_get_runs: Mock, job_request):
+    """Test verify returns instantly"""
+    rule = ToscaStitchRule(value=False)
+    rule.verify(job_request)
+    mock_get_runs.assert_not_called()
+
+
+@patch("rundetection.rules.tosca_rules.get_run_title")
+@patch("rundetection.rules.tosca_rules.Path.exists", return_value=True)
+def test_verify_should_stitch(_, mock_get_title: Mock, job_request):
+    """
+    Test the case where the previous run should stitch, but not the one prior
+    :param _:
+    :param mock_get_title: Mock get title
+    :param job_request: job request fixture
+    :return: None
+    """
+    # mock returns, the original title, the previous title (match), the one before the previous (no match)
+    mock_get_title.side_effect = [job_request.experiment_title, "experiment title run 2", "different experiment"]
+    rule = ToscaStitchRule(True)
+    rule.verify(job_request)
+
+    assert job_request.additional_requests[0].additional_values["input_runs"] == [12345, 12344]
