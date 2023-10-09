adfd4116abbe5242149c5cf1867c7351c0487145
Keiran Price <keiran.price@stfc.ac.uk>
2023-10-09 10:17:55 +0100

    break

--- rundetection/run_detection.py
+++ rundetection/run_detection.py
@@ -70,9 +70,7 @@ def process_messages(channel: BlockingChannel, notification_queue: SimpleQueue[J
             logger.warning("Problem processing message: %s", mf.body.decode)
         finally:
             channel.basic_ack(mf.delivery_tag)
-            time.sleep(0.1)
-            break
-    channel.cancel()
+        break
 
 
 def process_notifications(channel: BlockingChannel, notification_queue: SimpleQueue[JobRequest]) -> None:
