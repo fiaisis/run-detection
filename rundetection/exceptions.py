c60b5e3f2249c72a5f7404716307ffe6cea23c74
Keiran Price <keiran.price@stfc.ac.uk>
2023-12-04 20:48:16 +0000

    Add new exception

--- rundetection/exceptions.py
+++ rundetection/exceptions.py
@@ -7,3 +7,9 @@ class IngestError(Exception):
     """
     An unrecoverable error occurred when attempting to ingest a nexus file
     """
+
+
+class ReductionMetadataError(IngestError):
+    """
+    When metadata is incomplete or out out the expected form such that a reduction is not possible
+    """
