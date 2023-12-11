e75740efb8e555e3181eacb8fc155a23cce7d6b6
Keiran Price <keiran.price@stfc.ac.uk>
2023-12-11 08:24:33 +0000

    update tests

--- test/rules/test_osiris_rules.py
+++ test/rules/test_osiris_rules.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import pytest
 
 from rundetection.ingestion.ingest import JobRequest
-from rundetection.rules.osiris_rules import OsirisPanadiumRule
+from rundetection.rules.osiris_rules import OsirisPanadiumRule, OsirisReductionModeRule, OsirisStitchRule
 
 
 @pytest.fixture
@@ -43,6 +43,20 @@ def test_osiris_panadium_rule(job_request):
     assert job_request.additional_values["panadium"] == 12345
 
 
+def test_osiris_reduction_mode_freq_50_and_16(job_request):
+    """
+    Test defaults to spec for freq of 50 and 16
+    """
+    job_request.additional_values["freq10"] = 50
+    rule = OsirisReductionModeRule(True)
+    rule.verify(job_request)
+    assert job_request.additional_values["mode"] == "spectroscopy"
+    job_request.additional_values["freq10"] = 16
+    del job_request.additional_values["mode"]
+    rule.verify(job_request)
+    assert job_request.additional_values["mode"] == "spectroscopy"
+
+
 def test_osiris_stitch_rule(job_request):
     """
     Test runs are summed and additional request made
@@ -50,3 +64,13 @@ def test_osiris_stitch_rule(job_request):
     :return:
     """
     assert False, "Not implemented"
+
+
+def test_osiris_stitch_rule_will_do_nothing_if_diffraction_mode(job_request):
+    """
+    Test that sum runs will not be enabled for a diffraction run
+    """
+    job_request.additional_values["mode"] = "diffraction"
+    rule = OsirisStitchRule(True)
+    rule.verify(job_request)
+    assert job_request.additional_values["sum_runs"] is False
