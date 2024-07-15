e31fb1f1cebbadf3e5004cbb17cc2f8b2adca22f
Samuel Jones <samuel.jones@stfc.ac.uk>
2024-07-15 15:55:56 +0100

    Fix missed issue with factory

--- rundetection/rules/factory.py
+++ rundetection/rules/factory.py
@@ -8,7 +8,6 @@ from rundetection.rules.common_rules import EnabledRule
 from rundetection.rules.inter_rules import InterStitchRule
 from rundetection.rules.mari_rules import MariMaskFileRule, MariStitchRule, MariWBVANRule
 from rundetection.rules.osiris_rules import (
-    OsirisCalibrationRule,
     OsirisDefaultGraniteAnalyser,
     OsirisDefaultSpectroscopy,
     OsirisReductionModeRule,
@@ -60,9 +59,6 @@ def rule_factory(key_: str, value: T) -> Rule[Any]:  # noqa: C901, PLR0911, PLR0
         case "osirisreductionmode":
             if isinstance(value, bool):
                 return OsirisReductionModeRule(value)
-        case "osiriscalibration":
-            if isinstance(value, str):
-                return OsirisCalibrationRule(value)
         case _:
             raise MissingRuleError(f"Implementation of Rule: {key_} does not exist.")
 
