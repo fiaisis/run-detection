108a8db826ff485c92f65f98c53c7a2e9849dad3
Keiran Price <keiran.price@stfc.ac.uk>
2025-09-07 12:34:10 +0100

    Add mistakenly removed rule

--- rundetection/rules/enginx_rules.py
+++ rundetection/rules/enginx_rules.py
@@ -29,6 +29,25 @@ class EnginxVanadiumRunRule(Rule[int | str]):
         job_request.additional_values["vanadium_run"] = self._value
 
 
+class EnginxCeriaRunRule(Rule[int | str]):
+    """
+    Insert the ceria run number into the JobRequest
+
+    This value is used for calibration.
+    """
+
+    def verify(self, job_request: JobRequest) -> None:
+        """
+        Verify the rule against the job request.
+
+        Adds the ceria run number to the additional values.
+
+        :param job_request: The job request to verify.
+        :return: None.
+        """
+        job_request.additional_values["ceria_run"] = self._value
+
+
 class EnginxGroupRule(Rule[str]):
     """Insert the group type into the JobRequest"""
 
